3f913e6a630acea5733c1101fd980e7961d241b4
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 08:00:50 -0300

    refactor: observer sms notifier refactor

--- Engenharia03/Observer/Pattern/src/Observer/NotificarSMS.java
+++ Engenharia03/Observer/Pattern/src/Observer/NotificarSMS.java
@@ -1,6 +1,6 @@
 package Observer;
 
-public class NotificarSMS implements Observer{
+public class SMSNotifier implements Observer{
 
     public void update(){
         System.out.println("Notificar por SMS");
